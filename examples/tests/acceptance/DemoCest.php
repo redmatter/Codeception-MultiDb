c69c9d5a24b836134379f99a50c870787beb4f86
dino.korah <dino@redmatter.com>
2015-11-19 11:37:16 +0000

    Fixing syntax errors (sigh!)

--- examples/tests/acceptance/DemoCest.php
+++ examples/tests/acceptance/DemoCest.php
@@ -12,8 +12,16 @@ class DemoCest
 
         $user_id = $I->transaction(function () use ($I) {
             $I->executeSql('SELECT NOW()');
-            $user_id = $I->haveInDb('PrimaryDb.User', ['UserName'=>'someone@example.com', 'Name'=>'Some One', 'CreatedAt'=>'@asis NOW()']);
-            $I->haveInDb('PrimaryDb.UserPassword', ['UserID'=>$user_id, 'Password'=>'topsecret', 'CreatedAt'=>'@asis NOW()'])
+            $user_id = $I->haveInDb('PrimaryDb.User', [
+                'UserName'=>'someone@example.com',
+                'Name'=>'Some One',
+                'CreatedAt'=>'@asis NOW()'
+            ]);
+            $I->haveInDb('PrimaryDb.UserPassword', [
+                'UserID'=>$user_id,
+                'Password'=>'topsecret',
+                'CreatedAt'=>'@asis NOW()'
+            ]);
             
             return $user_id;
         });
