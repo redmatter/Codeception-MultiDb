d323b57c4c65e8cf6d3768aacc8bb853b629d4a2
Dino Korah <codemedic@users.noreply.github.com>
2015-11-19 10:56:40 +0000

    Update DemoCest.php

--- examples/tests/acceptance/DemoCest.php
+++ examples/tests/acceptance/DemoCest.php
@@ -10,8 +10,12 @@ class DemoCest
     {
         $I->amConnectedToDb('Primary');
 
-        $I->transaction(function () use ($I) {
+        $user_id = $I->transaction(function () use ($I) {
             $I->executeSql('SELECT NOW()');
+            $user_id = $I->haveInDb('PrimaryDb.User', ['UserName'=>'someone@example.com', 'Name'=>'Some One', 'CreatedAt'=>'@asis NOW()']);
+            $I->haveInDb('PrimaryDb.UserPassword', ['UserID'=>$user_id, 'Password'=>'topsecret', 'CreatedAt'=>'@asis NOW()'])
+            
+            return $user_id;
         });
     }
 }
