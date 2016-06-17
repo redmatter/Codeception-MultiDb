8a1deae78cdac897e870387d46e5007c116c6a3d
Dino Korah <codemedic@users.noreply.github.com>
2016-06-17 16:22:20 +0100

    Update user.sql

--- examples/tests/_data/user.sql
+++ examples/tests/_data/user.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE DemoConfig;
+CREATE DATABASE DemoWarehouse;
 CREATE USER 'demo'@'%' IDENTIFIED BY 'notsecure';
 GRANT ALL PRIVILEGES ON `DemoConfig`.* TO 'demo'@'%';
 GRANT ALL PRIVILEGES ON `DemoWarehouse`.* TO 'demo'@'%';
